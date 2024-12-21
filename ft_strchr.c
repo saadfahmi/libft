6a2c4163afe7670422a903d3880030bdcc6f936f
saadfahmitest <s.fahmi2618@uca.ac.ma>
2024-12-21 21:11:31 +0100

    mid

---
+++ ft_strchr.c
@@ -0,0 +1,16 @@
+#include "ft_libft.h"
+char ft_strchr(const char *s,int c )
+{
+    while(*s)
+    {
+        if(c == *s)
+        {
+            return((char *) s) ;
+
+        }
+        s++;
+
+    }
+    return(NULL);
+
+}
