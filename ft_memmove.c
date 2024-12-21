c7b02f97ec03620d00c2d14726b0fdb0060a2e3a
saadfahmitest <s.fahmi2618@uca.ac.ma>
2024-12-21 21:04:51 +0100

    mid

---
+++ ft_memmove.c
@@ -0,0 +1,33 @@
+#include "ft_libft.h"
+void ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *new_dest;
+    unsigned char *new_src;
+    size_t i = 0;
+    new_dest = (unsigned char *) dest;
+    new_src = (unsigned char *) src;
+
+    if(new_dest < new_src)
+    {
+        while(i < n)
+        {
+            new_dest[i] = new_src[i];
+            i++;
+
+        }
+
+    } else if(new_dest > new_src)
+    {
+        n = n -1;
+        while(i < n)
+        {
+            new_dest[n] = new_src[n];
+            n--;
+
+
+        }
+    }
+
+
+    return(new_dest);
+}
