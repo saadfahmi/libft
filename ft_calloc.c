9e93dc94ca35bb5cd19dae6ba86de4fc1eb9b042
saadfahmitest <s.fahmi2618@uca.ac.ma>
2024-12-23 18:29:02 +0100

    mid

---
+++ ft_calloc.c
@@ -0,0 +1,17 @@
+#include "ft_libft.h"
+
+void *calloc(size_t nitems,size_t size)
+{
+                void *arry;
+                size_t total;
+                total = size * nitems;
+                arry = malloc(total);
+        if(arry == NULL)
+        {
+            return(NULL);
+
+        }
+        ft_memset(arry, 0 ,total);
+        return(arry);
+        
+}
