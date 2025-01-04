6caccdbe187c8938dda70802a28f39396bf9116a
saadfahmitest <s.fahmi2618@uca.ac.ma>
2025-01-05 00:02:56 +0100

    mid

---
+++ ft_split.c
@@ -0,0 +1,90 @@
+#include "ft_libft.h"
+
+static char **malloc_arror(char **tab)
+{
+    unsigned int i;
+    i = 0;
+    while(tab[i])
+    
+            free(tab[i++]);
+            free(tab);
+            
+
+    return(NULL);
+}
+static int word_count(const char *str, char c)
+{
+            int i;
+            int count;
+            i = 0;
+            count = 0;
+            while(str[i])
+            {
+                if(str[i] == c)
+                i++;
+            
+            else
+            {
+                count++;
+                while(str[i] != c && str[i])
+                i++;
+
+            }
+            
+            }
+            return(count);
+
+            
+}
+static char *ft_getword(const char *s1, int *index, char c)
+{
+            char *copy;
+            size_t word_len;
+            int i;
+            word_len = 0;
+            while(s1[*index] == c)
+            (*index)++;
+            i = *index;
+            while(s1[i] &&s1[i] != c)
+            {
+                    word_len++;
+                    i++;
+
+            }
+            copy = malloc(sizeof(char) * (word_len + 1));
+            if(!copy)
+            return(NULL);
+            i = 0;
+            while (s1[*index] && s1[*index] != c)
+		            copy[i++] = s1[(*index)++];
+	        copy[i] = '\0';
+	        return (copy);
+            
+
+}
+char **ft_split(char const *s, char c)
+{
+        char **arr;
+        int index;
+        int wc;
+        int i;
+        index = 0;
+        i = 0;
+        if(!s)
+        return(NULL);
+        wc = word_count(s, c);
+        arr = malloc(sizeof(char *) * (wc + 1));
+        if(!arr)
+        retunr(NULL);
+        while(i < wc)
+        {
+                arr[i] = ft_getword(s, &index, c);
+                if(!arr[i])
+                retunr(malloc_arror(arr));
+                i++;
+
+        }
+        arr[i] = 0;
+	    return (arr);
+
+}
