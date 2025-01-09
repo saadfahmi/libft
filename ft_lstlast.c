fd2ffa46c693bd11a76603923fb2b46bbc318759
saadfahmitest <s.fahmi2618@uca.ac.ma>
2025-01-09 19:42:23 +0100

    mid

---
+++ ft_lstlast.c
@@ -0,0 +1,10 @@
+#include "ft_libft.h"
+t_list *ft_lstlast(t_list *lst)
+{
+        if(!lst)
+            return(NULL);
+        while(lst->next)
+            lst = lst->next;
+            return(lst);
+            
+}
