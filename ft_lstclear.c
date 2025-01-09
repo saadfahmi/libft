940b785458efaab91156243b2e8082fdefaa1043
saadfahmitest <s.fahmi2618@uca.ac.ma>
2025-01-09 19:39:43 +0100

    mid

---
+++ ft_lstclear.c
@@ -0,0 +1,13 @@
+#include "ft_libft.h"
+ void ft_lstclear(t_list **lst, void (*del)(void *))
+ {
+        t_list *tmp;
+
+        while(lst && *lst)
+        {
+                tmp = (*lst)->next;
+                ft_lstdelone(*lst, del);
+                *lst = tmp;
+                
+        }
+ }
